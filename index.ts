4f6c22caea65fb3633fa3c397e4528bd3024e2ec
William Thiery <williamalexthiery@hotmail.fr>
2020-12-06 13:22:15 +0100

    commit

---
+++ index.ts
@@ -0,0 +1,102 @@
+//Demonstration of Partial utility type.
+
+console.log("Demonstration of Partial utility type");
+
+type Rectangle = {
+    readonly width: number;
+    readonly height: number;
+}
+
+const updateRectangleDimensions = (rectangle: Rectangle, sideToUpdate: Partial<Rectangle>) =>{
+    return {...rectangle, ...sideToUpdate};
+}
+
+const rectangle: Rectangle = {
+    "height": 40,
+    "width": 50
+};
+
+
+console.log(`width: ${rectangle.width} - height: ${rectangle.height}`);
+
+const rectangle1 = updateRectangleDimensions(rectangle, {height: 10});
+//will return rectangle but with height set to 10.
+
+console.log(`width: ${rectangle1.width} - height: ${rectangle1.height}`);
+
+
+//Demonstration of Record utility type and interfaces.
+
+console.log("Demonstration of Record utility type and interfaces");
+
+interface Shape {
+    color: string;
+};
+
+interface Circle extends Shape{
+    diameter: number;
+}
+
+interface Rectangle_ extends Shape{
+    width: number;
+    height: number;
+};
+
+interface MyArray{
+    [key: number] : number;
+}
+
+const myArr: MyArray = [1,2,3,4]
+
+for(let i = 0; i < 4; i++){
+    console.log(myArr[i]);
+}
+    
+    
+
+const circle: Circle = {
+    "color" : "red",
+    "diameter": 10
+}
+
+const rectangle_: Rectangle_ = {
+    "color": "green",
+    "height": 10,
+    "width": 40
+}
+
+const sdf: Record<number, Rectangle_> = {
+    20: {
+        "color": "red",
+        "height": 123,
+        "width": 40
+    }
+}
+
+
+//Demonstration of Parameter Utility type
+
+declare function myFunction(n: number): void;
+
+
+type MyFunction = Parameters<
+    typeof myFunction
+>
+
+const x: MyFunction = [1];
+
+console.log(x);
+
+declare function myFunction2(args:{n: number; s: string}): void;
+
+const y: Parameters<typeof myFunction2> = [{
+    "n": 0,
+    "s": "hello"
+}]
+
+console.log(`n : ${y[0].n} s : ${y[0].s}`);
+
+
+
+
+
---
+++ js/index.js
@@ -0,0 +1,55 @@
+//Demonstration of Partial utility type.
+var __assign = (this && this.__assign) || function () {
+    __assign = Object.assign || function(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
+        }
+        return t;
+    };
+    return __assign.apply(this, arguments);
+};
+console.log("Demonstration of Partial utility type");
+var updateRectangleDimensions = function (rectangle, sideToUpdate) {
+    return __assign(__assign({}, rectangle), sideToUpdate);
+};
+var rectangle = {
+    "height": 40,
+    "width": 50
+};
+console.log("width: " + rectangle.width + " - height: " + rectangle.height);
+var rectangle1 = updateRectangleDimensions(rectangle, { height: 10 });
+//will return rectangle but with height set to 10.
+console.log("width: " + rectangle1.width + " - height: " + rectangle1.height);
+//Demonstration of Record utility type and interfaces.
+console.log("Demonstration of Record utility type and interfaces");
+;
+;
+var myArr = [1, 2, 3, 4];
+for (var i = 0; i < 4; i++) {
+    console.log(myArr[i]);
+}
+var circle = {
+    "color": "red",
+    "diameter": 10
+};
+var rectangle_ = {
+    "color": "green",
+    "height": 10,
+    "width": 40
+};
+var sdf = {
+    20: {
+        "color": "red",
+        "height": 123,
+        "width": 40
+    }
+};
+var x = [1];
+console.log(x);
+var y = [{
+        "n": 0,
+        "s": "hello"
+    }];
+console.log("n : " + y[0].n + " s : " + y[0].s);
